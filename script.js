fa27446c44c833bbf77e0d06197c72405746d51e
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-13 12:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function fibonacci(num) {
-  if (num <= 0) {
-    return { message: [] };
+  if (num === 0) {
+    return 0;
   } else if (num === 1) {
-    return { message: [0] };
-  } else if (num === 2) {
-    return { message: [0, 1] };
+    return 1;
   } else {
-    var sequence = [0, 1];
-    for (var i = 2; i < num; i++) {
-      var next = sequence[i - 1] + sequence[i - 2];
-      sequence.push(next);
+    let fibPrev = 0;
+    let fibCurr = 1;
+    let fibNext;
+
+    for (let i = 2; i <= num; i++) {
+      fibNext = fibPrev + fibCurr;
+      fibPrev = fibCurr;
+      fibCurr = fibNext;
     }
-    return { message: sequence };
+
+    return fibCurr;
   }
 }
+
 
 module.exports = fibonacci;
