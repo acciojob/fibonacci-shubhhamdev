e1f76e0a2150a95e6b34c8d5011badad963e6f0f
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-13 12:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function fibonacci(num) {
 // your code here
-	if (num <= 1) {
-    return num;
+	if (num <= 0) {
+    return [];
+  } else if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return [0, 1];
   } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
+    var sequence = [0, 1];
+    for (var i = 2; i < num; i++) {
+      var next = sequence[i - 1] + sequence[i - 2];
+      sequence.push(next);
+    }
+    return sequence;
   }
 }
 
